77de90aa8535c67a7b20c7049933aa7373da9dd8
Damian Barrios <barrios.d122@gmail.com>
2020-10-06 18:20:54 -0300

    configured connection to mongoose

---
+++ src/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const URI = process.env.MONGODB_URI 
+    ? process.env.MONGODB_URI 
+    : 'mongodb://localhost/databasetest' ;
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+ 
+mongoose.connect(URI)
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("DB is connected");
+});
