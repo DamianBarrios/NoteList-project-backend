1d6e16173e6abef31b06adca4768e8c8f0aa9a5e
Damian Barrios <barrios.d122@gmail.com>
2020-10-13 16:39:41 -0300

    fixed routes in users

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,13 +2,12 @@ const { Router } = require('express');
 const router = Router();
 
 const { getUsers , createUsers, deleteUsers } = require('../controllers/users.controller')
+
 router.route('/')
-    .get((req,res) => res.send("GET - Users Routes"))
-    .post((req,res) => res.send("POST - Users Routes"))
+    .get(getUsers)
+    .post(createUsers);
 
 router.route('/:id')
-    .get(getUsers)
-    .put(createUsers)
-    .delete(deleteUsers)     
+    .delete(deleteUsers);   
 
 module.exports = router;        
